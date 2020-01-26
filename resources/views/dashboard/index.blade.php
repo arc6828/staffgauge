6cb1ad5ee2caee3e23ba07bafdafd7088ea6802b
Rutsinsukmee <rutsin_sukmee@hotmail.com>
2020-01-26 20:28:10 +0700

    Update index.blade.php

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,107 +1,78 @@
-<!DOCTYPE html >
-  <head>
-    <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
-    <title>Using MySQL and PHP with Google Maps</title>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <style>
-      /* Always set the map height explicitly to define the size of the div
-       * element that contains the map. */
-      #map {
-        height: 100%;
-      }
-      /* Optional: Makes the sample page fill the window. */
-      html, body {
-        height: 100%;
-        margin: 0;
-        padding: 0;
-      }
-    </style>
-  </head>
+@extends('layout.main')
 
-<html>
-  <body>
-    <div id="map"></div>
+@section('content')
+    <script src="http://maps.google.com/maps/api/js?sensor=false"
+            type="text/javascript"></script>
+    <script type="text/javascript">
+        //<![CDATA[
 
-    <script>
-      var customLabel = {
-        restaurant: {
-          label: 'R'
-        },
-        bar: {
-          label: 'B'
+          var map;
+    var markers = {!! json_encode($markers) !!}; //this should dump a javascript array object which does not need any extra interperting.
+    var marks = []; //just incase you want to be able to manipulate this later
+
+    function load() {
+
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: 14.133982043026919, lng: 100.61786002773624},
+            zoom: 13
+        });           
+
+        for(var i = 0; i < markers.length; i++){
+            marks[i] = addMarker(markers[i]);
         }
-      };
+    }
 
-      function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          center: new google.maps.LatLng(14.133982043026919, 100.61786002773624),
-          zoom: 14
+    function addMarker(marker){
+        var sadrzaj = marker.anazivMarker};
+        var adresa = marker.adresa;
+        var grad = marker.nazivGrada;
+        var postanskibroj = marker.postanski_broj;
+        var zupanija = marker.nazivZupanije;
+
+        var html = "<b>" + sadrzaj + "</b> <br/>" + adresa +",<br/>"+postanskibroj+" "+grad+",<br/>"+zupanija;
+
+
+        var markerLatlng = new google.maps.LatLng(parseFloat(marker.lat),parseFloat(marker.lng));
+
+
+        var mark = new google.maps.Marker({
+            map: map,
+            position: markerLatlng,
+            icon: marker.slika
         });
+
         var infoWindow = new google.maps.InfoWindow;
+        google.maps.event.addListener(mark, 'click', function(){
+            infoWindow.setContent(html);
+            infoWindow.open(map, mark);
+        });
 
-          // Change this depending on the name of your PHP or XML file
-          $.getJSON('https://smartstaffgauge.com/api/map/staffgauges', function(err, data) {
+        return mark;
+    }
 
-            var json = data.responseJSON;
-            console.log(json);
+    function doNothing(){} //very appropriately named function. whats it for?
 
-            var markers = json.documentElement.getElementsByTagName('marker');
-            Array.prototype.forEach.call(markers, function(markerElem) {
-              var id = markerElem.getAttribute('id');
-              var name = markerElem.getAttribute('name');
-              var address = markerElem.getAttribute('address');
-              var type = markerElem.getAttribute('type');
-              var point = new google.maps.LatLng(
-                  parseFloat(markerElem.getAttribute('latitude')),
-                  parseFloat(markerElem.getAttribute('longitude')));
-
-              var infowincontent = document.createElement('div');
-              var strong = document.createElement('strong');
-              strong.textContent = name
-              infowincontent.appendChild(strong);
-              infowincontent.appendChild(document.createElement('br'));
-
-              var text = document.createElement('text');
-              text.textContent = address
-              infowincontent.appendChild(text);
-              var icon = customLabel[type] || {};
-              var marker = new google.maps.Marker({
-                map: map,
-                position: point,
-                label: icon.label
-              });
-              marker.addListener('click', function() {
-                infoWindow.setContent(infowincontent);
-                infoWindow.open(map, marker);
-              });
-            });
-          });
-        }
-
-
-
-        function getJSONP(url, success) {
-
-          var ud = '_' + +new Date,
-              script = document.createElement('script'),
-              head = document.getElementsByTagName('head')[0] 
-                    || document.documentElement;
-
-          window[ud] = function(data) {
-              head.removeChild(script);
-              success && success(data);
-          };
-
-          script.src = url.replace('callback=?', 'callback=' + ud);
-          head.appendChild(script);
-
-          }
-
-      function doNothing() {}
+        //]]>
     </script>
-    <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAG1_Wtq39qpBpTSaSne1jNv4GtMqIB920&callback=initMap">
-    </script>
-  </body>
-</html>
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dobro došli!</div>
+
+                <div class="panel-body">
+
+                    <!-- Moj kod  -->
+
+                    <div id="map"></div>
+
+
+
+
+                    <!-- DO TU -->
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
