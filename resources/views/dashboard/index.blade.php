bdab125c12fcb9f4b03d22a95d383832b78ef4ac
Rutsinsukmee <rutsin_sukmee@hotmail.com>
2020-01-29 21:38:06 +0700

    Update index.blade.php

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -128,7 +128,7 @@
         console.log('responseDate : ', responseDate);
 
         var data = new google.visualization.DataTable();
-          data.addColumn('date', 'Date');
+          data.addColumn('datetime', 'Date');
           data.addColumn('string', 'Level');
           data.addRows([
             [responseDate, ocr.title],
