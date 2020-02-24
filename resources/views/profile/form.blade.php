a4bc5e77077171d89dbd5f0582c6462b110c5b33
Rutsinsukmee <rutsin_sukmee@hotmail.com>
2020-02-24 17:48:45 +0700

    Update form.blade.php

--- resources/views/profile/form.blade.php
+++ resources/views/profile/form.blade.php
@@ -1,6 +1,6 @@
 <div class="form-group {{ $errors->has('role') ? 'has-error' : ''}}">
     <label for="role" class="control-label">{{ 'Role' }}</label>
-    <select class="form-control" name="role" type="text" id="role" value="{{ isset($profile->role) ? $profile->role : ''}}" ></select>
+    <select class="custom-select" name="role" type="text" id="role" value="{{ isset($profile->role) ? $profile->role : ''}}" ></select>
         <option value="admin">เจ้าหน้าที่</option>
         <option value="guest">สมาชิกทั่วไป</option>
     {!! $errors->first('role', '<p class="help-block">:message</p>') !!}
@@ -9,11 +9,6 @@
     <label for="user_id" class="control-label">{{ 'UserId' }}</label>
     <input class="form-control" name="user_id" type="number" id="user_id" value="{{ isset($profile->user_id) ? $profile->user_id : ''}}" readonly>
     {!! $errors->first('user_id', '<p class="help-block">:message</p>') !!}
-</div>
-<div class="form-group {{ $errors->has('lineid') ? 'has-error' : ''}}">
-    <label for="lineid" class="control-label">{{ 'LineId' }}</label>
-    <input class="form-control" name="lineid" type="number" id="lineid" value="{{ isset($profile->lineid) ? $profile->lineid : ''}}" readonly>
-    {!! $errors->first('lineid', '<p class="help-block">:message</p>') !!}
 </div>
 
 
