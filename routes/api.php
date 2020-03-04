dac6255636d9d31ed0cc483101381f79956454c5
Rutsinsukmee <rutsin_sukmee@hotmail.com>
2020-03-04 11:02:30 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -22,4 +22,4 @@ Route::resource('location', 'API\LocationController');
 Route::get('/map/locations','API\MapController@locations');
 Route::get('/map/staffgauges','API\MapController@staffgauges');
 Route::get('/map/ocrs','API\MapController@ocrs');
-Route::get('/updateocr', 'API\OcrController@updateocrs');
+Route::post('/updateocr', 'API\OcrController@updateocrs');
